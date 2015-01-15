b46d9e1a138c573b926cc8c7b808f6642d332406
Will <wrftaylor@gmail.com>
2015-01-15 21:47:38 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Driverhunt::Application.config.secret_key_base = 'f305d75a2d5091c994f6c87ae0ac7e1c98ec5ca7f020be14c3c56f9a854504b04975e61da68114778ac3a7ce995e507a8e87d8d35f5448221c7759a9b5bef032'
